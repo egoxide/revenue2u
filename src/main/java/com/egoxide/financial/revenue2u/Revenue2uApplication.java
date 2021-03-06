fd0f33a02ed12245bae03ca7575d6547be37cab1
Slava Egoroff <ego.spb@gmail.com>
2021-03-06 14:50:27 +0100

    Initial commit

---
+++ src/main/java/com/egoxide/financial/revenue2u/Revenue2uApplication.java
@@ -0,0 +1,13 @@
+package com.egoxide.financial.revenue2u;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Revenue2uApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Revenue2uApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/egoxide/financial/revenue2u/Revenue2uApplicationTests.java
@@ -0,0 +1,13 @@
+package com.egoxide.financial.revenue2u;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Revenue2uApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
